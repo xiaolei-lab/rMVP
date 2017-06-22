3c087b5ddf6a8670df17787a256cc8d1d2161cb8
XiaoleiLiuBio <xiaoleiliu@mail.hzau.edu.cn>
2017-06-22 16:07:11 +0800

    Update MVP.Version.r

--- R/MVP.Version.r
+++ R/MVP.Version.r
@@ -13,7 +13,7 @@ cat(paste("#", paste(rep("-", 35), collapse=""), "Welcome to MVP", paste(rep("-"
 
 cat("#", "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS #","\n")
 cat(paste("#   ", "Version: ", version, paste(rep(" ", 67), collapse=""), "#", sep=""), "\n")
-cat(paste("#  ", "Authors: Lilin Yin, Zhiwu Zhang, and Xiaolei Liu", paste(rep(" ", 31), collapse=""),"#", "\n"))
+cat(paste("#  ", "Authors: Lilin Yin, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu", paste(rep(" ", 31), collapse=""),"#", "\n"))
 cat(paste("#  ", "Contact: xiaoleiliu@mail.hzau.edu.cn", paste(rep(" ", 43), collapse=""), "#", "\n"))
-cat(paste("#", paste(rep("-", 84), collapse=""), "#", sep=""), "\n")
+cat(paste("#",  paste(rep("-", 84), collapse=""), "#", sep=""), "\n")
 }
