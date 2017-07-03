cd2670121f09216966b6354aa7decab3db111156
YinLiLin <1009926399@qq.com>
2017-07-03 16:53:09 +0800

    Update MVP.Version.r

--- R/MVP.Version.r
+++ R/MVP.Version.r
@@ -8,12 +8,12 @@ function(){
 # Last update: May 25, 2017
 ##############################################################################################
 version <- "0.1"
-
+authors <- "Lilin Yin, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu"
 cat(paste("#", paste(rep("-", 35), collapse=""), "Welcome to MVP", paste(rep("-", 35), collapse=""), "#",sep=""), "\n")
 
 cat("#", "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS #","\n")
-cat(paste("#   ", "Version: ", version, paste(rep(" ", 67), collapse=""), "#", sep=""), "\n")
-cat(paste("#  ", "Authors: Lilin Yin, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu", paste(rep(" ", 31), collapse=""),"#", "\n"))
+cat(paste("#   ", "Version: ", version, paste(rep(" ", 72-nchar(version)), collapse=""), "#", sep=""), "\n")
+cat(paste("#  ", "Authors:", authors, paste(rep(" ", 70-nchar(authors)), collapse=""),"#", "\n"))
 cat(paste("#  ", "Contact: xiaoleiliu@mail.hzau.edu.cn", paste(rep(" ", 43), collapse=""), "#", "\n"))
 cat(paste("#",  paste(rep("-", 84), collapse=""), "#", sep=""), "\n")
 }
