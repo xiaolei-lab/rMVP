0c82ec6ffa0c62e91255a1708a2a62db6b91a6d6
YinLiLin <1009926399@qq.com>
2017-09-13 10:22:20 +0800

    Update MVP.Version.r

--- R/MVP.Version.r
+++ R/MVP.Version.r
@@ -8,12 +8,13 @@ function(){
 # Last update: May 25, 2017
 ##############################################################################################
 version <- "0.1"
-authors <- "Lilin Yin, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu"
-cat(paste("#", paste(rep("-", 35), collapse=""), "Welcome to MVP", paste(rep("-", 35), collapse=""), "#",sep=""), "\n")
+authors <- "Lilin Yin, Haohao Zhang, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu"
+cat(paste("#", paste(rep("-", 38), collapse=""), "Welcome to MVP", paste(rep("-", 38), collapse=""), "#",sep=""), "\n")
 
-cat("#", "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS #","\n")
-cat(paste("#   ", "Version: ", version, paste(rep(" ", 72-nchar(version)), collapse=""), "#", sep=""), "\n")
-cat(paste("#  ", "Authors:", authors, paste(rep(" ", 70-nchar(authors)), collapse=""),"#", "\n"))
-cat(paste("#  ", "Contact: xiaoleiliu@mail.hzau.edu.cn", paste(rep(" ", 43), collapse=""), "#", "\n"))
-cat(paste("#",  paste(rep("-", 84), collapse=""), "#", sep=""), "\n")
+cat("#   ", "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS    #","\n")
+cat(paste("# ", "Version: ", version, paste(rep(" ", 80-nchar(version)), collapse=""), "#", sep=""), "\n")
+cat(paste("#", "Authors:", authors, paste(rep(" ", 78-nchar(authors)), collapse=""),"#", "\n"))
+cat(paste("#", "Contact: xiaoleiliu@mail.hzau.edu.cn", paste(rep(" ", 51), collapse=""), "#", "\n"))
+cat(paste("#",  paste(rep("-", 90), collapse=""), "#", sep=""), "\n")
 }
+
