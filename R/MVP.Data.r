530a98f1abac0db7b59fbf0466e1edbaba4725a9
Lilin Yin <1009926399@qq.com>
2017-11-11 20:34:41 +0800

    Update MVP.Data.r

--- R/MVP.Data.r
+++ R/MVP.Data.r
@@ -1,6 +1,6 @@
 MVP.Data <-
-function(fileHMP=NULL, fileBed=NULL, fileNum=NULL, filePhe=NULL, fileMap=NULL, fileKin=TRUE, filePC=TRUE, out=NULL, sep.hmp="\t", sep.num="\t", sep.map="\t", sep.phe="\t", sep.kin="\t", sep.pc="\t", type.geno="char", type.kin="double", type.pc="double", type.map="integer", SNP.effect="Add",
-SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed", perc=1, pcs.keep=5){
+function(fileVCF=NULL, fileHMP=NULL, fileBed=NULL, fileNum=NULL, filePhe=NULL, fileMap=NULL, fileKin=TRUE, filePC=TRUE, out=NULL, sep.vcf="\t", sep.hmp="\t", sep.num="\t", sep.map="\t", sep.phe="\t", sep.kin="\t", sep.pc="\t", vcf.jump=6, type.geno="char", type.kin="double", type.pc="double", type.map="integer", SNP.effect="Add",
+SNP.impute="Major", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed", perc=1, pcs.keep=5){
 ##########################################################################################################
 # Object: To prepare data for MVP package
 #
@@ -46,12 +46,14 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
     stop("Both Map and Numeric genotype files are needed!")
     if(is.null(out)) out="MVP"
     
+    #get id list from phenotype
     if(!is.null(filePhe)){
         myY <- read.delim(filePhe, sep=sep.phe, head = TRUE)
         taxa.y <- as.vector(myY[, 1])
         taxa <- taxa.y
     }
     
+    #convert bed/bim.fam to MVP
     if(!is.null(fileBed)){
         phe <- read.table(paste(fileBed, ".fam", sep=""), head=FALSE)[, -c(1,3:5)]
         write.table(phe, paste(out, ".phe", sep=""), row.names=FALSE, col.names=TRUE, sep=sep.phe, quote=FALSE)
@@ -71,19 +73,98 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
         dsc <- paste(out, ".geno.desc", sep="")
         nmarkers <- ncol(geno)
         ns <- nrow(geno)
-        myGeno.backed<-big.matrix(nmarkers, ns, type="char",
-        backingfile=bck, descriptorfile=dsc)
+        myGeno.backed <- big.matrix(nmarkers, ns, type="char", backingfile=bck, descriptorfile=dsc)
         options(bigmemory.typecast.warning=FALSE)
         #print("Output BIG genotype...")
-	inGENOFile=TRUE
+	    inGENOFile=TRUE
 		i <- 0
 		printN <- unlist(strsplit(x=as.character(nmarkers), split="", fixed=TRUE))
 		printIndex <- seq(0, (as.numeric(printN[1]) + 1) * 10^(length(printN)), 1000)[-1]
-		Num.fun <- function(x){
+		Num.fun <- function(x, impute="Middle"){
+		
 			x <- data.matrix(as.data.frame(x))
-			x[x==0]=2
+			
+			if(impute=="Middle"){
+				x[x==0] = 2
+			}
+			
+			if(impute=="Minor"){
+				lev=levels(as.factor(x))
+				lev=setdiff(lev,"0")
+				len=length(lev)
+				if(len==0){
+					minA = 2
+				}else if(len==1){
+					minA = lev[1]
+				}else if(len==2){
+					len.1 <- length(x[x==lev[1]])
+					len.2 <- length(x[x==lev[2]])
+					if(len.1<len.2){
+						minA = lev[1]
+					}else{
+						minA = lev[2]
+					}
+				}else if(len==3){
+					len.1 <- length(x[x==lev[1]])
+					len.2 <- length(x[x==lev[2]])
+					len.3 <- length(x[x==lev[3]])
+
+					min.all = min(len.1, len.2, len.3)
+					if(len.1 == min.all){
+						minA = lev[1]
+					}
+					if(len.2 == min.all){
+						minA = lev[2]
+					}
+					if(len.3 == min.all){
+						minA = lev[3]
+					}
+				}else if(len>3){
+					stop("More than three genotypes at a locus!")
+				}
+				x[x==0] = as.numeric(minA)
+			}
+			
+			if(impute=="Major"){
+				lev=levels(as.factor(x))
+				lev=setdiff(lev,"0")
+				len=length(lev)
+				if(len==0){
+					maxA = 2
+				}else if(len==1){
+					maxA = lev[1]
+				}else if(len==2){
+					len.1 <- length(x[x==lev[1]])
+					len.2 <- length(x[x==lev[2]])
+					if(len.1<len.2){
+						maxA = lev[2]
+					}else{
+						maxA = lev[1]
+					}
+				}else if(len==3){
+					len.1 <- length(x[x==lev[1]])
+					len.2 <- length(x[x==lev[2]])
+					len.3 <- length(x[x==lev[3]])
+
+					max.all = max(len.1, len.2, len.3)
+
+					if(len.1 == max.all){
+						maxA = lev[1]
+					}
+					if(len.2 == max.all){
+						maxA = lev[2]
+					}
+					if(len.3 == max.all){
+						maxA = lev[3]
+					}
+				}else if(len>3){
+					stop("More than three genotypes at a locus!")
+				}
+				x[x==0] = as.numeric(maxA)
+			}
 			return(x)
 		}
+		
 		while(inGENOFile){
 			i <- i + maxLine
 			if(i >= nmarkers){
@@ -97,9 +178,9 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
 				print(paste("Number of Markers Written into BIG File: ", xx, sep=""))
 			}
 			if(i >= nmarkers){
-				myGeno.backed [(i-maxLine + 1):nmarkers, ] <- -1 * apply(geno[, (i-maxLine + 1):nmarkers], 1, Num.fun) + 3
+				myGeno.backed [(i-maxLine + 1):nmarkers, ] <- -1 * apply(geno[, (i-maxLine + 1):nmarkers], 1,  function(x) Num.fun(x, impute=SNP.impute)) + 3
 			}else{
-				myGeno.backed [(i-maxLine + 1):i, ] <- -1 * apply(geno[, (i-maxLine + 1):i], 1, Num.fun) + 3
+				myGeno.backed [(i-maxLine + 1):i, ] <- -1 * apply(geno[, (i-maxLine + 1):i], 1, function(x) Num.fun(x, impute=SNP.impute)) + 3
 			}
 		}
         geno.flush <- flush(myGeno.backed)
@@ -110,10 +191,110 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
         }
     }
     
-    if(!is.null(fileHMP)){
-        #Output Y in same order with Genotype
-        #input phenotype
+    #Convert vcf to numeric
+    if(!is.null(fileVCF)){    
+        #get the first vcf
+        fileVCFCon <- file(description=fileVCF[1], open="r")
+        jj <- readLines(fileVCFCon, n=vcf.jump)
+        tt <- readLines(fileVCFCon, n=1, skipNul=1)
+        close.connection(fileVCFCon)
+        #tt2 <- unlist(strsplit(tt, sep.vcf))
+        tt3 <- unlist(strsplit(tt, sep.vcf))
+        tt2 <- unlist(strsplit(tt3, "-9_"))
+        taxa.g <- as.vector(tt2[-c(1:9)])
+        taxa.g <- taxa.g[taxa.g!=""]
+
+        if(is.null(filePhe)){
+            taxa=taxa.g
+        }else{
+            #Output Y in new order
+            taxa=intersect(taxa.g, taxa)
+            if(length(taxa) == 0){
+                print(paste("Phenotype individuals: ", paste(taxa.y[1:5], collapse=", "), "...", sep=""))
+                print(paste("Genotype individuals: ", paste(taxa.g[1:5], collapse=", "), "...", sep=""))
+                stop("No common individuals between phenotype and genotype!")
+            }
+            index = match(taxa, myY[, 1], nomatch = 0)
+            write.table(myY[index, ], file=paste(out, ".phe", sep=""), quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+            print("Preparation for PHENOTYPE data is done!")
+        }
+
+        #taxa <- intersect(taxa.g, taxa)
+        index=match(taxa, taxa.g, nomatch = 0)
+        ns = length(index)  #Number of individuals
+        #if(length(taxa) == 0){
+            #stop("No common individuals between phenotype and vcf file!")
+        #}
         
+        nFile=length(fileVCF)
+        
+        #Iteration among file
+        print("Numericilization...")
+        #if(priority == "memory"){
+            for (theFile in 1:nFile){
+                #Open VCF files
+                fileVCFCon <- file(description=fileVCF[theFile], open="r")
+                #Get VCF hearder
+                #index = taxa
+                #index = match(taxa, taxa.g, nomatch = 0)
+                #ns = length(index)  #Number of individuals
+                
+                #handler of first file
+                if(theFile == 1){
+                    #Open GD and GM file
+                    fileNumCon <- file(description=paste(out, ".numeric", sep=""), open="w")
+                    fileMapCon <- file(description=paste(out, ".map", sep=""), open="w")
+                    #GM header
+                    writeLines("SNP", fileMapCon, sep=sep.num)
+                    writeLines("Chrom", fileMapCon, sep=sep.num)
+                    writeLines("BP", fileMapCon, sep="\n")
+                }
+                
+                #Initialization for iteration within file
+                inFile=TRUE
+                i=0
+                jj <- readLines(fileVCFCon, n=vcf.jump+1)
+                #Iteration within file
+                while(inFile){
+                    i =i + 1
+                    if(i %% 1000 == 0) print(paste("Number of Markers Written into File: ", theFile, ": ", i, sep=""))
+
+                    tt <- readLines(fileVCFCon, n=1)
+                    tt2 <- unlist(strsplit(x=tt, split=sep.vcf, fixed=TRUE))
+                    
+                    #Identify end of file
+                    if(is.null(tt2[1])) inFile=FALSE
+                    if(i>maxRecord) inFile=FALSE
+                    
+                    if(inFile){
+                        #GM
+                        rs=tt2[3]
+                        chrom=tt2[1]
+                        pos=tt2[2]
+                        writeLines(as.character(rs), fileMapCon, sep=sep.num)
+                        writeLines(as.character(chrom), fileMapCon, sep=sep.num)
+                        writeLines(as.character(pos), fileMapCon, sep="\n")
+ 
+                        #GD
+                        GD <- VCF.Numeralization(x=tt2[-c(1:9)][index], impute=SNP.impute)
+                        writeLines(as.character(GD[1:(ns-1)]), fileNumCon, sep=sep.num)
+                        writeLines(as.character(GD[ns]), fileNumCon, sep="\n")
+                    }#enf of inFIle
+                } #end iteration within file
+                
+                #Close VCF file
+                close.connection(fileVCFCon)
+            } #end iteration among files
+            
+            #Close GD and GM file
+            close.connection(fileNumCon)
+            close.connection(fileMapCon)
+        #}
+        print("Preparation for numeric data is done!")
+    }#end of convert vcf to numeric
+
+    #Convert hapmap to numeric
+    if(!is.null(fileHMP)){
         #get the first hmp
         fileHMPCon<-file(description=fileHMP[1], open="r")
         #fileHMPCon<-file(description="hapmap3.hmp.txt", open="r")
@@ -170,9 +351,6 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
                     writeLines("SNP", fileMapCon, sep=sep.num)
                     writeLines("Chrom", fileMapCon, sep=sep.num)
                     writeLines("BP", fileMapCon, sep="\n")
-                    #GD header
-                    #writeLines(as.character(taxa[1:(ns-1)]), fileNumCon, sep=sep.num)
-                    #writeLines(as.character(taxa[ns]), fileNumCon, sep="\n")
                 }
                 
                 #Initialization for iteration within file
@@ -186,8 +364,8 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
                     tt2<-unlist(strsplit(x=tt, split=sep.hmp, fixed=TRUE))
                     
                     #Identify end of file
-                    if(is.null(tt2[1]))inFile=FALSE
-                    if(i>maxRecord)inFile=FALSE
+                    if(is.null(tt2[1])) inFile=FALSE
+                    if(i>maxRecord) inFile=FALSE
                     
                     if(inFile){
                         #GM
@@ -199,7 +377,7 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
                         writeLines(as.character(pos), fileMapCon, sep="\n")
                         
                         #GD
-                        GD= MVP.Numericalization(x=tt2[-c(1:11)], bit=bit, effect=SNP.effect, impute=SNP.impute)
+                        GD= HMP.Numeralization(x=tt2[-c(1:11)], bit=bit, effect=SNP.effect, impute=SNP.impute)
                         writeLines(as.character(GD[index[1:(ns-1)]]), fileNumCon, sep=sep.num)
                         writeLines(as.character(GD[index[ns]]), fileNumCon, sep="\n")
                     }#enf of inFIle
@@ -234,7 +412,7 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
                 nM <- nrow(myFile)
                 write.table(myFile[, c(1, 3, 4)], paste(out, ".map", sep=""), append=TRUE, col.names=FALSE, row.names=FALSE, quote=FALSE, sep=sep.num)
                 myFile <- myFile[, -c(1:11)];gc()
-                myGDx <- apply(myFile[, index], 1, function(x) MVP.Numericalization(x, bit=bit, effect=SNP.effect, impute=SNP.impute))
+                myGDx <- apply(myFile[, index], 1, function(x) HMP.Numeralization(x, bit=bit, effect=SNP.effect, impute=SNP.impute))
                 myGDx <- t(myGDx)
                 write.table(myGDx, paste(out, ".numeric", sep=""), append=TRUE, col.names=FALSE, row.names=FALSE, quote=FALSE, sep=sep.num)
                 rm(myFile);rm(myGDx);gc()
@@ -250,7 +428,7 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
     }
     
     #map
-    if((!is.null(fileMap))|(!is.null(fileHMP))){
+    if((!is.null(fileMap))|(!is.null(fileHMP))|(!is.null(fileVCF))){
     if(is.null(fileMap))	fileMap <- paste(out, ".map", sep="")
         myGM <- read.big.matrix(fileMap, type=type.map, sep=sep.map, head = TRUE)
         #backingfile=paste(out, ".map.bin", sep=""), descriptorfile=paste(out, ".map.desc", sep=""))
@@ -261,7 +439,7 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
         }
     
     #Transfer genotype data to .desc, .bin files
-    if((!is.null(fileNum))|(!is.null(fileHMP))){
+    if((!is.null(fileNum))|(!is.null(fileHMP))|(!is.null(fileVCF))){
         print("Output mvp genotype...")
         if(is.null(fileNum)) fileNum <- paste(out, ".numeric", sep="")
         # myGD <- read.big.matrix(fileNum, type=type.geno, sep=sep.num, head = FALSE,
@@ -381,7 +559,106 @@ SNP.impute="Middle", maxLine=10000, maxRecord=1e9, maxInd=1e9, priority="speed",
     print("MVP data prepration accomplished successfully!")
 }#end of MVP.Data function
 
-`MVP.Numericalization` <-
+VCF.Numeralization <- function(x, impute="Major"){
+    #Object: To convert vcf genotype to numeric data
+    #Input: x, a SNP vector
+    #Input: impute, "Major", "Minor", "Middle"
+    #Output: Coresponding numerical value
+    #Authors: Xiaolei Liu
+    #Last update: Nov 5, 2017
+    ##############################################################################################
+    x[x=="0/0"] = 0
+    x[x=="0/1"] = 1
+    x[x=="1/0"] = 1
+    x[x=="1/1"] = 2
+    x[x=="./1"] = "N"
+    x[x=="1/."] = "N"
+    x[x=="./0"] = "N"
+    x[x=="0/."] = "N"
+    x[x=="./."] = "N"
+
+    #Imputation for missing value
+    if(impute=="Middle"){
+        x[x=="N"] = 1
+    }
+    
+    if(impute=="Minor"){
+        n=length(x)
+        lev=levels(as.factor(x))
+        lev=setdiff(lev,"N")
+        len=length(lev)
+        if(len==0){
+            minA = 1
+        }else if(len==1){
+            minA = lev[1]
+        }else if(len==2){
+            len.1 <- length(x[x==lev[1]])
+            len.2 <- length(x[x==lev[2]])
+            if(len.1<len.2){
+                minA = lev[1]
+            }else{
+                minA = lev[2]
+            }
+        }else if(len==3){
+            len.1 <- length(x[x==lev[1]])
+            len.2 <- length(x[x==lev[2]])
+            len.3 <- length(x[x==lev[3]])
+
+            min.all = min(len.1, len.2, len.3)
+            if(len.1 == min.all){
+                minA = lev[1]
+            }
+            if(len.2 == min.all){
+                minA = lev[2]
+            }
+            if(len.3 == min.all){
+                minA = lev[3]
+            }
+        }
+        x[x=="N"] = minA
+    }
+    
+    if(impute=="Major"){
+        n=length(x)
+        lev=levels(as.factor(x))
+        lev=setdiff(lev,"N")
+        len=length(lev)
+        if(len==0){
+            maxA = 1
+        }else if(len==1){
+            maxA = lev[1]
+        }else if(len==2){
+            len.1 <- length(x[x==lev[1]])
+            len.2 <- length(x[x==lev[2]])
+            if(len.1<len.2){
+                maxA = lev[2]
+            }else{
+                maxA = lev[1]
+            }
+        }else if(len==3){
+            len.1 <- length(x[x==lev[1]])
+            len.2 <- length(x[x==lev[2]])
+            len.3 <- length(x[x==lev[3]])
+
+            max.all = max(len.1, len.2, len.3)
+
+            if(len.1 == max.all){
+                maxA = lev[1]
+            }
+            if(len.2 == max.all){
+                maxA = lev[2]
+            }
+            if(len.3 == max.all){
+                maxA = lev[3]
+            }
+        }
+        x[x=="N"] = maxA
+    }
+
+    return(x)
+}#end of `VCF.Numeralization`
+
+HMP.Numeralization <-
 function(x,bit=2,effect="Add",impute="None", Create.indicator = FALSE, Major.allele.zero = FALSE){
     #Object: To convert character SNP genotpe to numerical
     #Output: Coresponding numerical value
@@ -398,11 +675,21 @@ function(x,bit=2,effect="Add",impute="None", Create.indicator = FALSE, Major.all
     }
     
     if(bit==2)  {
-        x[x=="XX"]="N"
-        x[x=="--"]="N"
-        x[x=="++"]="N"
-        x[x=="//"]="N"
-        x[x=="NN"]="N"
+		#replace missing allels
+		x[x=="XX"]="N"
+		x[x=="--"]="N"
+		x[x=="++"]="N"
+		x[x=="//"]="N"
+		x[x=="NN"]="N"
+		x[x=="00"]="N"
+		
+		#replace false allels
+		x[x=="CA"]="AC"
+		x[x=="GA"]="AG"
+		x[x=="TA"]="AT"
+		x[x=="GC"]="CG"
+		x[x=="TC"]="CT"
+		x[x=="TG"]="GT"
     }
     
     n=length(x)
@@ -480,4 +767,4 @@ function(x,bit=2,effect="Add",impute="None", Create.indicator = FALSE, Major.all
     result=matrix(x,n,1)
 
     return(result)
-}#end of MVP.Numericalization function
+}
