b272bf8cd49af80d42b5510cab4e22d994279321
Lilin Yin <1009926399@qq.com>
2017-12-10 18:15:13 +0800

    Update MVP.Report.r

--- R/MVP.Report.r
+++ R/MVP.Report.r
@@ -1186,7 +1186,7 @@ MVP.Report <- function(
 									# cover the points that exceed the threshold with the color "white"
 									points(log.Quantiles[thre.index],log.P.values[thre.index], col = "white",pch=19,cex=cex[3])
 									if(is.null(signal.col)){
-										points(log.Quantiles[thre.index],log.P.values[thre.index],col = col[1],pch=signal.pch[1],cex=signal.cex[1])
+										points(log.Quantiles[thre.index],log.P.values[thre.index],col = t(col)[i],pch=signal.pch[1],cex=signal.cex[1])
 									}else{
 										points(log.Quantiles[thre.index],log.P.values[thre.index],col = signal.col[1],pch=signal.pch[1],cex=signal.cex[1])
 									}
