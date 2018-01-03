f863157bac0588aee52b95efe01993e86ff05b6b
Lilin Yin <1009926399@qq.com>
2018-01-03 16:05:48 +0800

    Update MVP.Version.r

--- R/MVP.Version.r
+++ R/MVP.Version.r
@@ -1,20 +1,25 @@
 MVP.Version <-
-function(){
+function(start=TRUE){
 ##############################################################################################
 # MVP: A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS
 # Designed by Xiaolei Liu
-# Writen by Lilin Yin, Zhiwu Zhang, and Xiaolei Liu
+# Writen by Lilin Yin
 # Build date: Aug 30, 2017
 # Last update: May 25, 2017
 ##############################################################################################
 version <- "1.0.1"
-authors <- "Lilin Yin, Haohao Zhang, Zhiwu Zhang, Xinyun Li, Shuhong Zhao, and Xiaolei Liu"
-cat(paste("#", paste(rep("-", 38), collapse=""), "Welcome to MVP", paste(rep("-", 38), collapse=""), "#",sep=""), "\n")
-
-cat("#   ", "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS    #","\n")
-cat(paste("# ", "Version: ", version, paste(rep(" ", 80-nchar(version)), collapse=""), "#", sep=""), "\n")
-cat(paste("#", "Authors:", authors, paste(rep(" ", 78-nchar(authors)), collapse=""),"#", "\n"))
-cat(paste("#", "Contact: xiaoleiliu@mail.hzau.edu.cn", paste(rep(" ", 51), collapse=""), "#", "\n"))
-cat(paste("#",  paste(rep("-", 90), collapse=""), "#", sep=""), "\n")
+authors <- "Lilin Yin, Haohao Zhang, Zhiwu Zhang, Xinyun Li, Xiaohui Yuan, Shuhong Zhao, and Xiaolei Liu"
+title <- "A Memory-efficient, Visualization-enhanced, and Parallel-accelerated Tool For GWAS"
+mail <- "xiaoleiliu@mail.hzau.edu.cn"
+Total.len <- nchar(authors)+11
+if(start){
+cat(paste("#", paste(rep("-", floor((Total.len-14)/2)), collapse=""), "Welcome to MVP", paste(rep("-", ceiling((Total.len-14)/2)), collapse=""), "#",sep=""), "\n")
+cat("#", paste(rep(" ", floor((Total.len-nchar(title))/2)-2), collapse=""), title, paste(rep(" ", ceiling((Total.len-nchar(title))/2-2)), collapse=""), "#", "\n")
+cat(paste("# ", "Version: ", version, paste(rep(" ", Total.len-nchar(version)-10), collapse=""), "#", sep=""), "\n")
+cat(paste("#", "Authors:", authors,"#", "\n"))
+cat(paste("#", "Contact:", mail, paste(rep(" ", Total.len-nchar(mail)-12), collapse=""), "#", "\n"))
+cat(paste("#",  paste(rep("-", Total.len), collapse=""), "#", sep=""), "\n")
+}else{
+cat(paste("#",  paste(rep("-", floor((Total.len-16)/2)), collapse=""), "MVP ACCOMPLISHED", paste(rep("-", ceiling((Total.len-16)/2)), collapse=""), "#", sep=""), "\n")
 }
-
+}
