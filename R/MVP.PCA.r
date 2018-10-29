244e3c9dcf9f591ba4da1f52cd551dd0c6bb470b
Haohao Zhang <hyacinth.hao@foxmail.com>
2018-10-29 14:41:57 +0800

    - Fixed maker sampling in PCA

--- R/MVP.PCA.r
+++ R/MVP.PCA.r
@@ -10,41 +10,41 @@ function(M, perc=1, pcs.keep=5, memo=NULL){
 #
 # Output:
 # PCs
-# Authors: Xiaolei Liu and Lilin Yin
-# Build date: Dec 14, 2016
+# Authors: Xiaolei Liu, Lilin Yin and Haohao Zhang
+# Build date: Oct 29, 2018
 # Last update: Dec 14, 2016
 ##########################################################################################################
     R.ver <- Sys.info()[['sysname']]
     wind <- R.ver == 'Windows'
     linux <- R.ver == 'Linux'
     mac <- (!linux) & (!wind)
-    r.open <- !inherits(try(Revo.version,silent=TRUE),"try-error")
+    r.open <- !inherits(try(Revo.version,silent = TRUE),"try-error")
 
-    if(r.open && mac){
+    if (r.open && mac) {
         Sys.setenv("VECLIB_MAXIMUM_THREADS" = "1")
     }
     #Data Check
-    if(is.null(M)){
+    if (is.null(M)) {
         stop("There is no genotype data!")
     }
 
-    if(pcs.keep < 0){
+    if (pcs.keep < 0) {
         pcs.keep = 0
     }
 
-    if(pcs.keep == 0){
+    if (pcs.keep == 0) {
         return(NULL)
     }
 
-    if(perc < 0 | perc > 1){
+    if (perc < 0 | perc > 1) {
         print("perc value should less than 1 and bigger than 0, MVP reset it to 1")
         perc <- 1
     }
     print("Principal Component Analysis Start...")
-    if(perc < 1){
+    if (perc < 1) {
         # set.seed(123456)
         m <- nrow(M)
-        big.geno <- M[sample(1:m, round(m * perc)), ]
+        big.geno <- t(M[sample(1:m, round(m * perc)), ])
     }else{
         big.geno <- t(as.matrix(M))
     }
@@ -52,5 +52,5 @@ function(M, perc=1, pcs.keep=5, memo=NULL){
 
     PCs <- prcomp(big.geno)$x[, 1:pcs.keep]
 
-    return(list(PCs=PCs))
+    return(list(PCs = PCs))
 }#end of MVP.PCA function
