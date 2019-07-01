88b37e742a3ba32c02e52078b6e97073f96a49c2
Lilin Yin <1009926399@qq.com>
2019-07-01 10:15:10 +1000

    Update MVP.MLM.r

--- R/MVP.MLM.r
+++ R/MVP.MLM.r
@@ -25,16 +25,13 @@
 #' @param phe phenotype, n * 2 matrix
 #' @param geno genotype, m * n, m is marker size, n is population size
 #' @param K Kinship, Covariance matrix(n * n) for random effects; must be positive semi-definite
+#' @param eigenK list of eigen Kinship
 #' @param CV covariates
 #' @param REML a list that contains ve and vg
-#' @param priority speed or memory
 #' @param cpu number of cpus used for parallel computation
 #' @param bar whether to show the progress bar
-#' @param vc.method the methods for estimating variance component("emma" or "gemma")
-#' @param maxLine when the priority is 'memory', users can change this parameter to limit the memory
-#' @param file.output whether to output files or not
-#' @param memo a marker on output file name
-#'
+#' @param vc.method the methods for estimating variance component("emma" or "he" or "brent")
+
 #' @return
 #' results: a m * 2 matrix, the first column is the SNP effect, the second column is the P values
 #' @export
@@ -52,40 +49,46 @@
 #' K <- MVP.K.VanRaden(genotype)
 #' mlm <- MVP.MLM(phe=phenotype, geno=genotype, K=K)
 #' str(mlm)
+
 MVP.MLM <-
-function(phe, geno, K=NULL, CV=NULL, REML=NULL, priority="speed", cpu=1, bar=TRUE,vc.method="EMMA",maxLine=1000, file.output=TRUE, memo="MVP"){
-    if (Sys.info()[['sysname']] == 'Windows')
-        cpu <- 1
-    math.cpu <- try(getMKLthreads(), silent=TRUE)
+function(
+    phe, 
+    geno, 
+    K=NULL,
+    eigenK=NULL,
+    CV=NULL, 
+    REML=NULL,
+    cpu=1,
+    bar=TRUE,
+    vc.method=c("BRENT", "EMMA", "HE")
+){
+    R.ver <- Sys.info()[['sysname']]
+    r.open <- !inherits(try(Revo.version,silent=TRUE),"try-error")
     
+    if (R.ver == 'Windows') cpu <- 1
+    if (r.open && cpu > 1 && R.ver == 'Darwin') {
+        Sys.setenv("VECLIB_MAXIMUM_THREADS" = "1")
+    }
+
+    math.cpu <- try(getMKLthreads(), silent=TRUE)
+    vc.method <- match.arg(vc.method)
     n <- ncol(geno)
     m <- nrow(geno)
-    if(priority=="speed") geno <- as.matrix(geno)
     
     ys <- as.numeric(as.matrix(phe[,2]))
-    if (is.null(K)) {
-        print("Calculating Kinship...")
-        K <- MVP.K.VanRaden(M=geno, priority=priority, maxLine=maxLine);gc()
-        if (file.output) {
-            filebck <- paste0("MVP.", colnames(phe)[2], memo, ".kin.bin")
-            filedes <- paste0("MVP.", colnames(phe)[2], memo, ".kin.desc")
-            if (file.exists(filebck)) file.remove(filebck)
-            if (file.exists(filedes)) file.remove(filedes)
-            Kin.backed <- big.matrix(
-                nrow = nrow(K),
-                ncol = ncol(K),
-                type = "double",
-                backingfile = filebck,
-                descriptorfile = filedes
-            )
-            Kin.backed[, ] <- K[, ]
-            flush(Kin.backed)
-            rm(list = c("Kin.backed"))
-            gc()
+    if(is.null(K)){
+        if(vc.method == "EMMA" | vc.method == "he")  stop("Kinship must be provided!")
+        if(vc.method == "BRENT"){
+            if(is.null(eigenK)) stop("eigenK must be provided!")
         }
-    } else {
+    }else{
         # convert K to base:matrix
         K <- K[, ]
+        if(is.null(eigenK)){
+            print("Eigen Decomposition of Kinship...")
+            eigenK <- eigen(K, symmetric=TRUE)
+            print("Eigen-Decomposition is Done!")
+        }
     }
 
     if (is.null(CV)) {
@@ -96,14 +99,16 @@ function(phe, geno, K=NULL, CV=NULL, REML=NULL, priority="speed", cpu=1, bar=TRU
 
     # number of fixed effects
     nf <- ncol(X0) + 1
-    if (is.null(REML)) {
-        print("Variance components...")   
-        if (vc.method == "EMMA")
-            REML <- MVP.EMMA.Vg.Ve(y=ys, X=X0, K=K)
-        if (vc.method == "GEMMA")
-            REML <- MVP.GEMMA.Vg.Ve(y=ys, X=X0, K=K)
-        print("Variance components is Done!")
+    if(is.null(REML)) {
+		print(paste("Variance components using: ", vc.method), sep="")   
+        if (vc.method == "EMMA") REML <- MVP.EMMA.Vg.Ve(y=ys, X=X0, K=K)
+        if (vc.method == "HE") REML <- MVP.HE.Vg.Ve(y=ys, X=X0, K=K)
+        if (vc.method == "BRENT") REML <- MVP.BRENT.Vg.Ve(y=ys, X=X0, eigenK=eigenK)
+		print(paste("Estimated Vg and Ve: ", sprintf("%.6f", REML$vg), " ", sprintf("%.6f", REML$ve), sep=""))
+    }else{
+        print(paste("Provided Vg and Ve: ", sprintf("%.6f", REML$vg), " ", sprintf("%.6f", REML$ve), sep=""))
     }
+    if(!is.null(K)){rm(K); gc()}
 
     q0 <- ncol(X0)
     iXX <- matrix(NA,q0+1,q0+1)
@@ -112,29 +117,20 @@ function(phe, geno, K=NULL, CV=NULL, REML=NULL, priority="speed", cpu=1, bar=TRU
     ves <- REML$ve
     vgs <- REML$vg
     lambda <- ves/vgs
-    print("Eigen Decomposition...")
-    eig <- eigen(K, symmetric=TRUE); rm(K); gc()
-    print("Eigen-Decomposition is Done!")
-    
-    U <- eig$vectors * matrix(sqrt(1/(eig$values + lambda)), n, length(eig$values), byrow=TRUE); rm(eig); gc()
+
+    U <- eigenK$vectors * matrix(sqrt(1/(eigenK$values + lambda)), n, length(eigenK$values), byrow=TRUE); rm(eigenK); gc()
     
     y <- matrix(ys)
     yt <- crossprod(U, y)
     X0t <- crossprod(U, X0)
     X0X0 <- crossprod(X0t)
     X0Y <- crossprod(X0t,yt)
-    iX0X0 <- solve(X0X0)
-    
+    iX0X0 <- solve(X0X0)    
     Xt[1:n,1:q0] <- X0t
 
     #parallel function for MLM model
     eff.mlm.parallel <- function(i){
         if(bar) print.f(i)
-        # if(i%%1000==0){
-            # print(paste("****************", i, "****************",sep=""))
-        # }
-        #if(cpu>1 & r.open) setMKLthreads(math.cpu)
-
         SNP <- geno[i, ]
         xst <- crossprod(U, SNP)
         Xt[1:n,q0+1] <- xst
@@ -143,37 +139,34 @@ function(phe, geno, K=NULL, CV=NULL, REML=NULL, priority="speed", cpu=1, bar=TRU
         xstxst <- crossprod(xst)
         xsY <- crossprod(xst,yt)
         XY <- c(X0Y,xsY)
-        #B22 <- xstxst - XstX0%*%iX0X0%*%X0Xst
         B22 <- xstxst - crossprod(X0Xst, iX0X0) %*% X0Xst
         invB22 <- 1/B22
         B21 <- tcrossprod(XstX0, iX0X0)
         NeginvB22B21 <- crossprod(-invB22,B21)
-        B11 <- iX0X0 + as.numeric(invB22)*crossprod(B21,B21)
-            
+        B11 <- iX0X0 + as.numeric(invB22)*crossprod(B21,B21)    
         iXX[1:q0,1:q0]=B11
         iXX[(q0+1),(q0+1)]=1/B22
         iXX[(q0+1),1:q0]=NeginvB22B21
         iXX[1:q0,(q0+1)]=NeginvB22B21
         beta <- crossprod(iXX,XY)
-        stats <- beta[(q0+1)]/sqrt((iXX[(q0+1), (q0+1)]) * vgs)
+        se <- sqrt((iXX[(q0+1), (q0+1)]) * vgs)
+        stats <- beta[(q0+1)]/se
         p <- 2 * pt(abs(stats), n-(q0+1), lower.tail=FALSE)
         effect<- beta[(q0+1)]
-        return(list(effect = effect, p = p))
+        return(list(effect = effect, se = se, p = p))
     }
     
     #Paralleled MLM
-    if (cpu == 1) {
-        print.f <- function(i) {
-            print_bar(i = i, n = m, type = "type1", fixed.points = TRUE)
-        }
-        mkl_env({
-            results <- lapply(seq_len(m), eff.mlm.parallel)
-        }, threads = min(2^(n %/% 1000), math.cpu))
-    } else {
-        if (Sys.info()[['sysname']] == 'Windows'){
-            print.f <- function(i) {
-                print_bar(i = i, n = m, type = "type1", fixed.points = TRUE)
-            }
+    if(cpu == 1){
+        math.cpu <- try(getMKLthreads(), silent=TRUE)
+        mkl.cpu <- ifelse((2^(n %/% 1000)) < math.cpu, 2^(n %/% 1000), math.cpu)
+        try(setMKLthreads(mkl.cpu), silent=TRUE)
+        print.f <- function(i){print_bar(i=i, n=m, type="type1", fixed.points=TRUE)}
+        results <- lapply(1:m, eff.mlm.parallel)
+        try(setMKLthreads(math.cpu), silent=TRUE)
+    }else{
+        if(R.ver == 'Windows'){
+            print.f <- function(i){print_bar(i=i, n=m, type="type1", fixed.points=TRUE)}
             cl <- makeCluster(getOption("cl.cores", cpu))
             clusterExport(cl, varlist=c("geno", "yt", "X0", "U", "vgs", "ves", "math.cpu"), envir=environment())
             Exp.packages <- clusterEvalQ(cl, c(library(bigmemory)))
@@ -184,20 +177,13 @@ function(phe, geno, K=NULL, CV=NULL, REML=NULL, priority="speed", cpu=1, bar=TRU
             tmpf <- fifo(tmpf.name, open="w+b", blocking=TRUE)
             writeBin(0, tmpf)
             print.f <- function(i){print_bar(n=m, type="type3", tmp.file=tmpf, fixed.points=TRUE)}
-            R.ver <- Sys.info()[['sysname']]
-            if(R.ver == 'Linux') {
-                math.cpu <- try(getMKLthreads(), silent=TRUE)
-                try(setMKLthreads(1), silent=TRUE)
-            }
-            results <- mclapply(1:m, eff.mlm.parallel, mc.cores=cpu)
-            if(R.ver == 'Linux') {
-                try(setMKLthreads(math.cpu), silent=TRUE)
-                #try(setMKLthreads(1), silent=TRUE)
-            }
+            mkl_env({
+                results <- mclapply(1:m, eff.mlm.parallel, mc.cores=cpu)
+            })
             close(tmpf); unlink(tmpf.name); cat('\n');
         }
     }
     if(is.list(results)) results <- matrix(unlist(results), m, byrow=TRUE)
-    #print("****************MLM ACCOMPLISHED****************")
     return(results)
 }#end of MVP.MLM function
+
