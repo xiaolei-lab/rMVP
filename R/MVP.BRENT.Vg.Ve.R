622241aaeb02f294dda15c0047210e7db7abfcb7
Lilin Yin <1009926399@qq.com>
2019-07-03 15:59:19 +1000

    Update MVP.BRENT.Vg.Ve.R

--- R/MVP.BRENT.Vg.Ve.R
+++ R/MVP.BRENT.Vg.Ve.R
@@ -1,7 +1,15 @@
 MVP.BRENT.Vg.Ve <-
 function(y, X, eigenK)
 {
-    reml <- brent(Y=y, X=X, eigenK=eigenK, verbose=FALSE)
+    p=0
+    Sigma <- eigenK$values
+    w <- which(Sigma < 1e-6)
+    Sigma[w] <- 1e-6
+    U <- eigenK$vectors
+    min_h2 = 0
+    max_h2 = 1
+    tol = .Machine$double.eps^0.25
+    reml <- brent(y, X, p, Sigma, U, min_h2, max_h2, tol, FALSE, TRUE)
     vg <- reml[[2]]
     ve <- reml[[1]]
     delta <- ve / vg
