8c7f2011462df8b4921ba12af921bbd704164e68
Lilin Yin <1009926399@qq.com>
2019-07-04 21:46:02 +1000

    Update kinship.cpp

--- src/kinship.cpp
+++ src/kinship.cpp
@@ -9,7 +9,7 @@
 #include <bigmemory/MatrixAccessor.hpp>
 #include <R_ext/Print.h>
 #include <progress.hpp>
-#include <progress_bar.hpp>
+#include <progress/progress_bar.hpp>
 
 // [[Rcpp::plugins(cpp11)]]
 // [[Rcpp::depends(RcppArmadillo)]]
