ea257c327ab847ec8ece58b0e4c1a2af2f273551
Haohao Zhang <hyacz@foxmail.com>
2019-08-22 14:02:08 +0800

    - add new logging module

---
+++ R/MVP.Logging.R
@@ -0,0 +1,42 @@
+# Copyright (C) 2016-2018 by Xiaolei Lab
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+logging.initialize <- function(module) {
+    file <- NULL
+    if (options("rMVP.OutputLog2File") == TRUE) {
+        now <- Sys.time()
+        file <- paste(module, format(now, "%Y%m%d_%H%M%S"), "log", sep = ".")
+    }
+    
+    assign("logging.file", file, envir = globalenv())
+}
+
+
+logging.log <- function(..., file = NULL, sep = " ", fill = FALSE, labels = NULL, verbose = NULL) {
+    if (is.null(verbose)) {
+        verbose <- options("rMVP.Verbose")[[1]]
+    }
+    if (verbose) {
+        cat(..., sep = sep, fill = fill, labels = labels)
+    }
+    
+    if (is.null(file)) {
+        file <- get("logging.file", envir = globalenv())
+    }
+    
+    if (!is.null(file)) {
+        cat(..., file = file, sep = sep, fill = fill, labels = labels, append = TRUE)
+    }
+}
