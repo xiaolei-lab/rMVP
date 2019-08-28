63f6e8429f71476de74b32d794bebdaa3888c32a
Haohao Zhang <hyacz@foxmail.com>
2019-08-28 19:39:08 +0800

    - delete file copyright

--- R/MVP.Utility.r
+++ R/MVP.Utility.r
@@ -1,5 +1,3 @@
-# Copyright (C) 2016-2018 by Xiaolei Lab
-# 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
