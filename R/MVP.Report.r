824a7d0f2d99e2f0426ad37484a2c72405011695
Lilin Yin <1009926399@qq.com>
2020-05-10 17:21:16 +1000

    Update MVP.Report.r

--- R/MVP.Report.r
+++ R/MVP.Report.r
@@ -200,7 +200,7 @@ MVP.Report <- function(
 		map <- as.matrix(map)
 		map <- map[!is.na(map[, 2]), ]
 		map <- map[!is.na(as.numeric(map[, 3])), ]
-		map <- map[map[, 2] != 0, ]
+		map <- map[as.numeric(map[, 2]) != 0, ]
 		#map <- map[map[, 3] != 0, ]
 		suppressWarnings(max.chr <- max(as.numeric(map[, 2]), na.rm=TRUE))
 		if(is.infinite(max.chr))	max.chr <- 0
@@ -362,6 +362,9 @@ MVP.Report <- function(
 		#order Pmap by the name of SNP
 		#Pmap=Pmap[order(Pmap[,1]),]
 		Pmap <- as.matrix(Pmap)
+		Pmap <- Pmap[!is.na(Pmap[, 2]), ]
+        Pmap <- Pmap[!is.na(as.numeric(Pmap[, 3])), ]
+        Pmap <- Pmap[as.numeric(Pmap[, 2]) != 0, ]
 
 		#scale and adjust the parameters
 		cir.chr.h <- cir.chr.h/5
